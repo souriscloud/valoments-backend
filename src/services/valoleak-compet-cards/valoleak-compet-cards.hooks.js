19595d40eabefd7bc588d12ee61b44423d73aa16
Souris.CLOUD <me@souris.cloud>
2021-01-08 00:34:59 +0100

    createorupdate hook (valoleak-compet-cards)

--- src/services/valoleak-compet-cards/valoleak-compet-cards.hooks.js
+++ src/services/valoleak-compet-cards/valoleak-compet-cards.hooks.js
@@ -5,10 +5,27 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      async hookContext => {
+        const existQuery = await hookContext.service.find({ query: { userId: hookContext.data.userId } })
+        if (existQuery.total === 1) {
+          hookContext.statusCode = 201
+          hookContext.result = await hookContext.service.update(existQuery.data[0]._id, hookContext.data)
+        }
+        
+        return hookContext
+      }
+    ],
     update: [],
     patch: [],
-    remove: []
+    remove: [
+      // async hookContext => {
+      //   let response
+      //   try {
+      //     response = await hookContext.ser
+      //   }
+      // }
+    ]
   },
 
   after: {
